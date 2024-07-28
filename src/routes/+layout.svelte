e7352309e030c69dce92d7732f7dd9494575a8e8
undonepotato <icyseas@outlook.com>
2024-07-28 15:24:37 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+	<title>Experiment 🧪</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<meta name="author" content="Hayden Kwon" />
+	<meta name="description" content="A small website for experiment testing" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<slot></slot>
+
+<style>
+	:root {
+		--background: #f0f1fb;
+		--text: #080b1c;
+		--primary: #485bcd;
+		--secondary: #e399ce;
+		--accent: #d56483;
+		font-family: 'Inter', Arial, Helvetica, sans-serif;
+		color: var(--text);
+	}
+
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		font-family: 'Poppins', 'Inter', Arial, Helvetica, sans-serif;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<div id="title-div">
+	<h1 id="title-heading">Learning Curve Experiment</h1>
+</div>
+
+<div id="tests-div">
+	<a href="tests/a" class="test-button" id="test-button-a">Test A</a>
+	<a href="tests/b" class="test-button" id="test-button-b">Test B</a>
+	<a href="tests/c" class="test-button" id="test-button-c">Test C</a>
+</div>
+
+<style>
+	:root {
+		overflow: hidden;
+	}
+
+	#title-div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		block-size: calc(100vh - 5rem);
+	}
+
+	#title-heading {
+		font-size: 3rem;
+		text-align: center;
+		padding-inline: 2rem;
+	}
+
+	#tests-div {
+		display: flex;
+		justify-content: center;
+		gap: 10px;
+	}
+
+	.test-button {
+		flex: 1;
+
+		block-size: 4rem;
+		border-radius: 10px;
+		border: none;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		text-decoration: none;
+	}
+
+	#test-button-a {
+		color: var(--background);
+		background: var(--primary);
+	}
+
+	#test-button-b {
+		color: var(--text);
+		background: var(--secondary);
+	}
+
+	#test-button-c {
+		color: var(--text);
+		background: var(--accent);
+	}
+</style>
